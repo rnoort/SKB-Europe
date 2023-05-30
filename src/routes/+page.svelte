91d459007f4d98bac6f6ed07ebba3fe56e63cc2f
Robijn Noort <rrnoort@gmail.com>
2023-05-30 22:39:33 +0200

    feat: added page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import NewsEvents from "$lib/NewsEvents.svelte";
+    import Applications from "$lib/Applications.svelte";
+    import Reviews from "$lib/Reviews.svelte";
+</script>
+
+
+<Applications />
+<Reviews />
+<NewsEvents />
